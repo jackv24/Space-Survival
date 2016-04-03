eadf4808481256893602d418d2f9bae6405ecd9f
Jack Vine <jackv949@gmail.com>
2016-04-03 19:29:41 +0930

    Started inventory.

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    public int slots = 12;
+}
---
+++ Assets/Scripts/UI/DisplayInventory.cs
@@ -0,0 +1,34 @@
+﻿/*
+**  Author: Jack Vine
+**
+**  Displays the inventory slots. Assumed a grid layout group exists.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public GameObject slotPrefab;
+
+    void Start()
+    {
+        Inventory inventory = GameObject.FindWithTag("Player").GetComponent<Inventory>();
+
+        if (inventory)
+        {
+            int slotsCount = inventory.slots;
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+
+            for (int i = 0; i < slotsCount; i++)
+            {
+                GameObject obj = Instantiate(slotPrefab);
+                obj.transform.SetParent(transform, false);
+            }
+        }
+    }
+}
